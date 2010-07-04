50f252eab29637f10a372827fe1d5b2d3178700c
Francisco Prates <ilusia.dev@gmail.com>
2010-07-04 17:23:56 -0300

    controller: transporte de dados do formulario para objeto para model.save().
    Signed-off-by: Francisco Prates <ilusia.dev@gmail.com>

--- org/eve/sd/customer/CustomerController.java
+++ org/eve/sd/customer/CustomerController.java
@@ -3,6 +3,7 @@ package org.eve.sd.customer;
 import org.eve.main.EVE;
 import org.eve.model.Model;
 import org.eve.view.AbstractController;
+import org.eve.view.Form;
 
 public class CustomerController extends AbstractController {
     
@@ -10,11 +11,19 @@ public class CustomerController extends AbstractController {
     public final void userInput(String input) {
         Customer customer = (Customer)getObject();
         Model model = getModel();
+        Form form = getForm("main");
         
         if (input.equals("customer.save")) {
             try {
+                customer.setAlternateName(form.getString("customer.aname"));
+                customer.setCodCadNac(form.getString("customer.cnpj"));
+                customer.setId(form.getInt("customer.ident"));
+                customer.setName(form.getString("customer.name"));
+                customer.setStatus(form.getInt("customer.status"));
+                
                 model.save(customer);
-                getForm("main").setInt("customer.ident", customer.getId());
+                
+                form.setInt("customer.ident", customer.getId());
                 
                 setMessage(EVE.status, "customer.save.successful");
             } catch (Exception ev) {                
