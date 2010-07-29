e3fc1ce80b7919abaafb612564cc6e1234c645a4
Francisco Prates <ilusia.dev@gmail.com>
2010-07-28 21:57:47 -0300

    sd.supplier: implementaçao inicial de tela de seleçao.
    Signed-off-by: Francisco Prates <ilusia.dev@gmail.com>

--- org/eve/sd/supplier/view/SupplierSelectionView.java
+++ org/eve/sd/supplier/view/SupplierSelectionView.java
@@ -3,8 +3,12 @@
  */
 package org.eve.sd.supplier.view;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eve.view.AbstractView;
+import org.eve.view.Form;
 
 /**
  * @author francisco.prates
@@ -21,9 +25,29 @@ public class SupplierSelectionView extends AbstractView {
      * @see org.eve.ui.View#defineForm()
      */
     @Override
-    public void defineView(Composite container) {        
+    public void defineView(Composite container) {
+        Group selpor;
+        Form form = addForm("main");
+        Form selporform = addForm("selpor");
+        
+        container.setLayout(new RowLayout(SWT.VERTICAL));
+
         addAction("supplier.show.sel");
         addAction("supplier.edit.sel");
+        
+        form.setLocale(getLocale());
+        form.put("supplier.ident", 12);        
+        form.define(container);
+        
+        selpor = new Group(container, SWT.SHADOW_IN);
+        selporform.setLocale(getLocale());
+        selpor.setLayout(new RowLayout(SWT.VERTICAL));
+        selpor.setText(getMessage("supplier.select.by"));
+        
+        selporform.put("supplier.name", 40);
+        selporform.put("supplier.aname", 40);
+        selporform.define(selpor);
+        selpor.pack();
         
         addButton("supplier.sel");
     }
